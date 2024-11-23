d564753e801afc2736cceba306e9b760dbdb0c04
harsha200210 <harshadilanbb2020@gmail.com>
2024-11-23 21:55:21 +0530

    Add properties file

--- src/main/java/lk/ijse/db/FactoryConfiguration.java
+++ src/main/java/lk/ijse/db/FactoryConfiguration.java
@@ -8,17 +8,30 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+
 public class FactoryConfiguration {
     private static FactoryConfiguration factoryConfiguration;
     private SessionFactory sessionFactory;
 
     private FactoryConfiguration() {
-        Configuration configuration = new Configuration().configure()
-                .addAnnotatedClass(User.class)
-                .addAnnotatedClass(CulinaryProgram.class)
-                .addAnnotatedClass(Enrollment.class)
-                .addAnnotatedClass(Student.class);
-        sessionFactory = configuration.buildSessionFactory();
+        try {
+            Properties properties = new Properties();
+            FileInputStream input = new FileInputStream("src/main/resources/hibernate.properties");
+            properties.load(input);
+            Configuration configuration = new Configuration();
+            configuration.setProperties(properties)
+                    .addAnnotatedClass(User.class)
+                    .addAnnotatedClass(CulinaryProgram.class)
+                    .addAnnotatedClass(Enrollment.class)
+                    .addAnnotatedClass(Student.class);
+            sessionFactory = configuration.buildSessionFactory();
+        } catch (Exception e){
+
+        }
     }
 
     public Session getSession() {
